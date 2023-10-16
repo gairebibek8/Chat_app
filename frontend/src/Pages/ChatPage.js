92905886c86b334ae9605c8ffeb48ceed090326d
gairebibek8 <gairebibek8@gmail.com>
2023-10-16 22:43:22 +0545

    created pages

---
+++ frontend/src/Pages/ChatPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ChatPage = () => {
+
+const fetchChats = () => {
+    
+}
+
+  return (
+    <div>
+      Chat Page
+    </div>
+  )
+}
+
+export default ChatPage
